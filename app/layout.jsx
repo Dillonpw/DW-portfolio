917706d4bf482ef5faceaaf4586ad522d6a2a2c8
dillonpw <dillonpwalsh10@gmail.com>
2024-04-26 10:52:15 -0400

    moved component

--- app/layout.jsx
+++ app/layout.jsx
@@ -77,8 +77,9 @@ export default function RootLayout({ children }) {
       <body className="relative min-h-full bg-gradient-to-br from-gray-300 to-emerald-400 p-0 text-slate-900 dark:bg-gradient-to-br dark:from-sky-950 dark:to-slate-900 dark:text-cyan-200">
         {children}
         <Footer />
-      </body>
       <Analytics />
+
+      </body>
     </html>
   );
 }
