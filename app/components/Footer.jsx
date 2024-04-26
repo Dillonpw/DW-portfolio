25978005f33801cd183c2470beb0c1471ec24f38
dillonpw <dillonpwalsh10@gmail.com>
2024-04-26 10:37:05 -0400

    updated footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <>
       <footer className="flex flex-col items-center justify-center bg-emerald-700 pb-6 pt-10 text-center text-sm text-white dark:bg-sky-950">
         <p>
-          Built using <b>React</b> and <b>Tailwind</b>, with some elements
+          Built using <b>React</b> <b>Next.JS</b> and <b>Tailwind</b>, with some elements
           designed in Figma, Deployed through Vercel.
           <br />
           Created by Dillon Walsh &copy;2024 All Rights Reserved
