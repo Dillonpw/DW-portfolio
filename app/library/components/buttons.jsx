ec26782114d913936edc4e98aaf8412c8cacb6ec
dillonpw <dillonpwalsh10@gmail.com>
2024-04-30 13:49:44 -0400

    button basic outline

---
+++ app/library/components/buttons.jsx
@@ -0,0 +1,20 @@
+const Buttons = () => {
+  {
+    /* Use custom classes to style your buttons/ give title to each style*/
+  }
+
+  return (
+    <div className="flex gap-4">
+      <button>Button</button>
+      <button>Button</button>
+      <button>Button</button>
+      <button>Button</button>
+      <button>Button</button>
+      <button>Button</button>
+      <button>Button</button>
+      <button>Button</button>
+      <button>Button</button>
+      <button>Button</button>
+    </div>
+  );
+};
