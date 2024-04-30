a7d37ff14cec0f49a6cd6dfeb89604595ecad04c
dillonpw <dillonpwalsh10@gmail.com>
2024-04-30 19:04:28 -0400

    added input boilerplate

---
+++ app/library/components/inputs.jsx
@@ -0,0 +1,20 @@
+const Inputs = () => {
+    {
+      /* Use custom classes to style your inputs/ give title to each style*/
+    }
+  
+    return (
+      <div className="flex gap-4">
+        <input>Button</input>
+        <input>Button</input>
+        <input>Button</input>
+        <input>Button</input>
+        <input>Button</input>
+        <input>Button</input>
+        <input>Button</input>
+        <input>Button</input>
+        <input>Button</input>
+        <input>Button</input>
+      </div>
+    );
+  };
