b5d795247dd2090682ee209584564c41ad0b39c1
dillonpw <dillonpwalsh10@gmail.com>
2024-06-21 21:56:50 -0400

    trying to correct prerender

--- app/components/theme.jsx
+++ app/components/theme.jsx
@@ -1,22 +1,27 @@
-"use client";
 import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 
 const ThemeToggle = () => {
-  const [darkMode, setDarkMode] = useState(() => {
-    return localStorage.getItem("theme") === "dark";
-  });
+  const [darkMode, setDarkMode] = useState(false);
 
   useEffect(() => {
-    document.documentElement.classList.toggle("dark", darkMode);
-  }, [darkMode]);
+    if (typeof window !== "undefined" && window.localStorage) {
+      const storedTheme = localStorage.getItem("theme");
+      if (storedTheme === "dark") {
+        setDarkMode(true);
+        document.documentElement.classList.add("dark");
+      }
+    }
+  }, []);
 
   const toggleTheme = () => {
     const isDarkMode = !darkMode;
     setDarkMode(isDarkMode);
-    localStorage.setItem("theme", isDarkMode ? "dark" : "light");
-    document.documentElement.classList.toggle("dark", isDarkMode);
+    if (typeof window !== "undefined" && window.localStorage) {
+      localStorage.setItem("theme", isDarkMode ? "dark" : "light");
+      document.documentElement.classList.toggle("dark", isDarkMode);
+    }
   };
 
   return (
@@ -30,7 +35,6 @@ const ThemeToggle = () => {
           icon={darkMode ? faSun : faMoon}
         />
       </button>
-
     </div>
   );
 };
