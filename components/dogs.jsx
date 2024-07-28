fde8c78fd8c27fb15efddab3dd6b71f136d9f920
dillonpw <dillonpwalsh10@gmail.com>
2024-07-28 16:25:07 -0400

    using tooltip for dog easter egg

--- components/dogs.jsx
+++ components/dogs.jsx
@@ -1,29 +1,36 @@
-import { useState } from "react";
 import Image from "next/image";
-const Dogs = () => {
-  const [isHovered, setIsHovered] = useState(false);
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
+const Dogs = () => {
   return (
-    <div className="mb-8 h-[110px]">
-      <h3
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-        className="fade-in-3 cursor-pointer text-xl opacity-60"
-      >
-        I&apos;m probably walking my dogs
-      </h3>
-      <div className="flex flex-row items-center justify-center">
-        {isHovered && (
-          <Image
-            className="rounded-xl border-2 border-neutral-950"
-            src="/Ollie-benny.webp"
-            width={140}
-            height={140}
-            alt="my dogs Ollie and Benny"
-          ></Image>
-        )}
-      </div>
-    </div>
+    <>
+      <TooltipProvider className="mb-8 h-[110px]">
+        <Tooltip>
+          <TooltipTrigger>
+            <h3 className="fade-in-3 cursor-pointer text-xl opacity-60">
+              I&apos;m probably walking my dogs
+            </h3>
+          </TooltipTrigger>
+          <TooltipContent
+            side="bottom"
+            className="flex flex-row items-center justify-center"
+          >
+            <Image
+              className="rounded-md border-none"
+              src="/Ollie-benny.webp"
+              width={160}
+              height={160}
+              alt="my dogs Ollie and Benny"
+            ></Image>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </>
   );
 };
 export default Dogs;
