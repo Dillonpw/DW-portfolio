0d6f14cc15271c492e6e41cb71662357e99ffa8b
dillonpw <dillonpwalsh10@gmail.com>
2024-10-07 16:29:11 -0400

    added new project

--- components/Images.jsx
+++ components/Images.jsx
@@ -14,13 +14,64 @@ const images = [
       "Shadcn/ui",
       "Auth.js",
       "Drizzle ORM",
-      "Neon Postrgres",
+      "Neon Postgres",
       "Playwright",
     ],
     challenge:
-      "This project was fun getting deeper into the server side, working with a new ORM and preparing to provide an actual utility tool, for now the product remains free with stripe donation options but a paid version may come down the pipleline if it becomes too expensive to manage. I referenced several previouds projects to make sure I was on the right track throughout development as well as relevant documentation.",
+      "This project was fun getting deeper into the server side, working with a new ORM and preparing to provide an actual utility tool. For now, the product remains free with Stripe donation options, but a paid version may come down the pipeline if it becomes too expensive to manage. I referenced several previous projects to make sure I was on the right track throughout development, as well as relevant documentation.",
     about:
-      "A free schedule generator app for people who work rotation shifts based on a 24-hour basis instead of a Monday-Friday schedule. This app accept input and returns a schedule based on the type of rotation the user needs to bulid around, with output displayed in either a list format or calendar view. This project allows for either Google log-in, Github or magic link log in with email address.",
+      "A free schedule generator app for people who work rotation shifts based on a 24-hour basis instead of a Monday-Friday schedule. This app accepts input and returns a schedule based on the type of rotation the user needs to build around, with output displayed in either a list format or calendar view. This project allows for either Google login, GitHub, or magic link login with an email address.",
+  },
+  {
+    id: "ecommerce-site",
+    src: "/ecommerce.png",
+    src2: "/ecommerce-2.png",
+    mobileImg: "/ecommerce-m.png",
+    livePreviewLink: "https://shop-red-eta.vercel.app/",
+    githubLink: "https://github.com/Dillonpw/ecommerce",
+    title: "Ecommerce Site",
+    tags: ["TypeScript", "React", "Tailwind CSS", "TailwindUI", "Shadcn/ui"],
+    challenge:
+      "This project was rather large, and with that came some challenges: for one, I struggled a bit with sharing state between components and pages. I failed to realize that the concept I was inventing in my head and almost in my code already existed in the form of useContext. Once that was out of the way, I found some difficulty in positioning certain elements exactly where I wanted them so they looked presentable both on desktop and mobile, but a few Tailwind size adjustments sorted that out.",
+    about:
+      "Engaging in this project introduced me to many new concepts within React. I dove headfirst into the useContext hook and scoured the React documentation to gain what I believe is a working grasp of its uses. In the end, I am happy with the outcome. The goal was to create something purposely generic and minimal, and I believe that’s what I have achieved. You will notice a checkout area if you decide to go through the motions of the website; it does not submit anything, it just clears the cart.",
+  },
+  {
+    id: "FEM-LP1",
+    src: "/home-dt.jpeg",
+    mobileImg: "/home-m.jpeg",
+    livePreviewLink: "homefem-dillonpws-projects.vercel.app",
+    githubLink: "https://github.com/Dillonpw/FEM-LP1",
+    title: "Design File To Code",
+    tags: ["JavaScript", "React", "Tailwind CSS", "Vite"],
+    challenge:
+      "The only challenge I ran into with this simple design-to-code project was after designing mobile first with Flexbox. In order to get the layout I needed for desktop, I had to rebuild using Grid instead. No huge issues with the transition, but it did set me back a bit. The purpose of this project for me was to test how quickly and how closely I could take a design file and produce a finished project.",
+    about:
+      "This project was taken from Frontendmentor.io, supplying the design and expected outcome. I used Tailwind and a few React components to bring the design file to life, in a form as close to pixel-perfect as I could get without excruciating adjustments. The project took approximately 3 hours of work from download to finished project. The time was mostly due to the experience I have with the tools I used.",
+  },
+  {
+    id: "small-bis",
+    src: "/small-bis.png",
+    mobileImg: "/small-bis-m.png",
+    livePreviewLink: "https://t1.dillonwalsh.com/",
+    githubLink: "https://github.com/Dillonpw/small-bis",
+    title: "Small Business Template",
+    tags: ["TypeScript", "React", "Tailwind CSS", "Astro", "Playwright"],
+    challenge:
+      "This project was another valuable learning experience in using Astro, my favorite framework for static site generation and server-side rendering. One of the primary challenges was ensuring the UI design was both precise and aesthetically pleasing, which required attention to detail and careful planning. Additionally, integrating TypeScript and ensuring the template was fully responsive across different devices were significant tasks that demanded thorough testing and iteration.",
+    about:
+      "This is a fully customizable website template designed specifically for small businesses. It features a simple and clean layout, with a built-in light/dark mode toggle to cater to user preferences. The template is fully responsive whether it's a desktop, tablet, or smartphone. The template is designed to serve as a starting point for small business websites, allowing clients to personalize the content and style to meet their specific needs. It showcases potential design ideas for clients, demonstrating how their business can have a professional and attractive online presence.",
+  },
+  {
+    id: "Pomodoro Timer",
+    src: "/pomodoro.png",
+    githubLink: "https://github.com/Dillonpw/pomodoro",
+    title: "Pomodoro Timer Desktop Application",
+    tags: ["TypeScript", "React", "Tailwind CSS", "Tauri", "Shadcn/ui"],
+    challenge:
+      "This was my first project using Tauri, and it involved some learning curves around the build and deployment processes. Despite these challenges, it was an exciting learning experience. I learned a lot about desktop app development, and while the initial setup was a bit confusing, I was ultimately able to create a functional application with a great deal of personal utility.",
+    about:
+      "A simple yet effective Pomodoro Timer desktop application built with Tauri, designed to help users improve productivity by following the Pomodoro technique. The app offers an intuitive UI, customizable time intervals, and an efficient timer mechanism. It was a great introduction to desktop application development, and it serves as a useful tool for staying focused and productive throughout the workday.",
   },
   {
     id: "recipe-book",
@@ -41,48 +92,9 @@ const images = [
       "Neon Postgres",
     ],
     challenge:
-      "One of my first real full stack applications. relied heavily on official docs for guidance with the API routes, authentication, Database management and schemas. Styles are farily generic, but the point of the project wasn't to be a beautiful app, it was to make a utility tool. ",
+      "One of my first real full stack applications. I relied heavily on official docs for guidance with the API routes, authentication, database management, and schemas. Styles are fairly generic, but the point of the project wasn't to be a beautiful app; it was to make a utility tool.",
     about:
-      "A full stack application built with next.js app router and several other tools for both frontend and backend efficiency, the recipe book allows users to log in using OAuth, store their personal recipes, find other peoples recipes and use this app to read, find and store recipes without the clutter of ads and blog posts on recipe websites.",
-  },
-  {
-    id: "ecommerce-site",
-    src: "/ecommerce.png",
-    src2: "/ecommerce-2.png",
-    mobileImg: "/ecommerce-m.png",
-    livePreviewLink: "https://shop-red-eta.vercel.app/",
-    githubLink: "https://github.com/Dillonpw/ecommerce",
-    title: "Ecommerce Site",
-    tags: ["TypeScript", "React", "Tailwind CSS", "TailwindUI", "Shadcn/ui"],
-    challenge:
-      "This project was rather large and with that came some challenges: for one I struggled a bit with sharing state between components and pages, I failed to realize that the concept I was inventing in my head and almost in my code already existed in the form of useContex... Once that was out of the way I found some difficulty in positioniing certain elements exactly where I wanted them so they looked presentable both on desktop and mobile but a few tailwind size adjustements sorted that out.",
-    about:
-      "You will notice a checkout area if you decide to go through te motions of the website, it does not submit anything, it clears the cart. Engaging in this project introduced me to many new concepts within React. I dove headfirst into the useContext hook and scoured the React documentation to gain what I believe is a working grasp of its uses. In the end, I am happy with the outcome. The goal was to create something purposely generic and minimal, and I believe that’s what I have achieved.",
-  },
-
-  {
-    id: "small-bis",
-    src: "/small-bis.png",
-    mobileImg: "/small-bis-m.png",
-    livePreviewLink: "https://t1.dillonwalsh.com/",
-    githubLink: "https://github.com/Dillonpw/small-bis",
-    title: "Small Business Template",
-    tags: ["TypeScript", "React", "Tailwind CSS", "Astro", "Playwright"],
-    challenge:
-      "This project was another valuable learning experience in using Astro, my favorite framework for static site generation and server-side rendering. One of the primary challenges was ensuring the UI design was both precise and aesthetically pleasing, which required attention to detail and careful planning. Additionally, integrating TypeScript and ensuring the template was fully responsive across different devices were significant tasks that demanded thorough testing and iteration.",
-    about:
-      "This is a fully customizable website template designed specifically for small businesses. It features a simple and clean layout, with a built-in light/dark mode toggle to cater to user preferences. The template is fully responsive whether it's a desktop, tablet, or smartphone. The template sis designed to serve as a starting point for small business websites, allowing clients to personalize the content and style to meet their specific needs. It showcases potential design ideas for clients, demonstrating how their business can have a professional and attractive online presence.",
-  },
-  {
-    id: "Pomodoro Timer",
-    src: "/pomodoro.png",
-    githubLink: "https://github.com/Dillonpw/pomodoro",
-    title: "Pomodoro Timer Desktop Application",
-    tags: ["TypeScript", "React", "Tailwind CSS", "Tauri", "Shadcn/ui"],
-    challenge:
-      "This was my first project using Tauri, and it involved some learning curves around the build and deployment processes. Despite these challenges, it was an exciting learning experience. I learned a lot about desktop app development, and while the initial setup was a bit confusing, I was ultimately able to create a functional application with a great deal of personal utlilty.",
-    about:
-      "A simple yet effective Pomodoro Timer desktop application built with Tauri, designed to help users improve productivity by following the Pomodoro technique. The app offers an intuitive UI, customizable time intervals, and an efficient timer mechanism. It was a great introduction to desktop application development, and it serves as a useful tool for staying focused and productive throughout the workday.",
+      "A full stack application built with Next.js app router and several other tools for both frontend and backend efficiency. The recipe book allows users to log in using OAuth, store their personal recipes, find other people's recipes, and use this app to read, find, and store recipes without the clutter of ads and blog posts on recipe websites.",
   },
 ];
 
