8599a1e67db3a7321b7604187961b0be465a43e1
dillonpw <dillonpwalsh10@gmail.com>
2024-10-13 15:31:53 -0400

    formatting

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -6,12 +6,12 @@ export default function Blog() {
   return (
     <section>
       <NavBar>
-        <h1 className="font-mono text-4xl">
+        <h1 className="text-4xl">
           <Link href="/blog">All Posts</Link>
         </h1>
       </NavBar>
-      <div className="m-10 my-40 flex w-fit cursor-pointer flex-col items-center justify-center hover:scale-105">
-        <Link className="text-center" href="/blog/ticker">
+      <div className="m-10 my-40 flex w-fit cursor-pointer flex-col items-center justify-center">
+        <Link className="text-center hover:underline" href="/blog/ticker">
           <Image
             className="rounded-xl"
             src="/scroll-component.png"
@@ -19,8 +19,9 @@ export default function Blog() {
             height={150}
             alt="scroll component"
           />
-          Super Simple News Ticker with Tailwind
+          Simple News Ticker with Tailwind
         </Link>
+        <p className="font-light italic opacity-60">Dillon Walsh 10/13/24</p>
       </div>
     </section>
   );
