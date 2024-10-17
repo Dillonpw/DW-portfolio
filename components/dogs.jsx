81ed97aa157c842161eee1ed550d7cd04c73bb4d
dillonpw <dillonpwalsh10@gmail.com>
2024-10-17 13:34:22 -0400

    update dogs easter egg

--- components/dogs.jsx
+++ components/dogs.jsx
@@ -8,29 +8,28 @@ import {
 
 const Dogs = () => {
   return (
-    <>
-      <TooltipProvider>
-        <Tooltip>
-          <TooltipTrigger>
-            <h4 className="fade-in-3 cursor-pointer text-xl opacity-60">
-              I&apos;m probably walking my dogs
-            </h4>
-          </TooltipTrigger>
-          <TooltipContent
-            side="bottom"
-            className="flex flex-row items-center justify-center"
-          >
-            <Image
-              className="rounded-md border-none"
-              src="/Ollie-benny.webp"
-              width={160}
-              height={160}
-              alt="my dogs Ollie and Benny"
-            ></Image>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
-    </>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>
+          <h4 className="fade-in-3 cursor-pointer text-xl opacity-60">
+            I&apos;m probably walking my dogs
+          </h4>
+        </TooltipTrigger>
+        <TooltipContent
+          side="bottom"
+          className="border-none bg-transparent p-0"
+        >
+          <Image
+            className="rounded-md" 
+            src="/Ollie-benny.webp"
+            width={180}
+            height={180}
+            alt="my dogs Ollie and Benny"
+          />
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 };
+
 export default Dogs;
