dc3a46ffe1b1bc8d088b98857e963166daea7549
dillonpw <dillonpwalsh10@gmail.com>
2024-10-20 16:18:11 -0400

    seo updates

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,29 +1,44 @@
-export default function sitemap() {
-  return [
-    {
-      url: "https://dillonwalsh.com",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://t1.dillonwalsh.com",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://blog.dillonwalsh.com",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://dillonwalsh.com/projects",
-      lastModified: new Date(),
-    },
+import { getAllProjects } from '@/lib/projects'
+import { getAllPosts } from '@/lib/posts'
 
-    {
-      url: "https://dillonwalsh.com/robots.txt",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://dillonwalsh.com/sitemap.xml",
-      lastModified: new Date(),
-    },
-  ];
+export default async function sitemap() {
+  const baseUrl = 'https://dillonwalsh.com'
+
+  // Fetch all projects
+  const projects = await getAllProjects()
+  const projectUrls = projects.map((project) => ({
+    url: `${baseUrl}/projects/${project.id}`,
+    lastModified: new Date(project.updatedAt),
+    changeFrequency: 'monthly',
+    priority: 0.8,
+  }))
+
+  // Fetch all blog posts (if you have a blog)
+  const posts = await getAllPosts()
+  const postUrls = posts.map((post) => ({
+    url: `${baseUrl}/blog/${post.slug}`,
+    lastModified: new Date(post.date),
+    changeFrequency: 'weekly',
+    priority: 0.7,
+  }))
+
+  // Define static routes
+  const routes = [
+    '',
+    'projects',
+    't1',
+    'robots.txt',
+    'sitemap.xml'
+  ].map((route) => ({
+    url: `${baseUrl}${route ? `/${route}` : ''}`,
+    lastModified: new Date(),
+    changeFrequency: 'daily',
+    priority: 1,
+  }))
+
+  return [
+    ...routes,
+    ...projectUrls,
+    ...postUrls,
+  ]
 }
