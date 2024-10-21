4a42e47b45dd8ccc120f7aeb2d8d3816329a0c5b
dillonpw <dillonpwalsh10@gmail.com>
2024-10-21 14:30:25 -0400

    update sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,19 +1,28 @@
 import { getAllProjects } from "/lib/projects";
 import { getAllPosts } from "/lib/blogPosts";
+import images from "/components/Images.jsx"; 
 
 export default async function sitemap() {
   const baseUrl = "https://dillonwalsh.com";
 
-  // Fetch all projects
   const projects = await getAllProjects();
-  const projectUrls = projects.map((project) => ({
-    url: `${baseUrl}/projects/${project.id}`,
-    lastModified: new Date(project.updatedAt),
-    changeFrequency: "monthly",
-    priority: 0.8,
-  }));
+  
+  const projectUrls = projects.map((project) => {
+    const projectImage = images.find((img) => img.id === project.id);
 
-  // Fetch all blog posts (if you have a blog)
+    return {
+      url: `${baseUrl}/projects/${project.id}`,
+      lastModified: new Date(project.updatedAt),
+      changeFrequency: "monthly",
+      priority: 0.8,
+      images: projectImage ? [{
+        loc: `${baseUrl}${projectImage.src}`,
+        title: projectImage.title,
+      }] : [],
+    };
+  });
+
+  // Fetch all blog posts
   const posts = await getAllPosts();
   const postUrls = posts.map((post) => ({
     url: `${baseUrl}/blog/${post.slug}`,
