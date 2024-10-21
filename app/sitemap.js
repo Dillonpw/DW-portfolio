10d81a7a181c623c14c9fad07e3c29c38ce71996
dillonpw <dillonpwalsh10@gmail.com>
2024-10-21 15:05:56 -0400

    updated sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,28 +1,22 @@
-import { getAllProjects } from "/lib/projects";
 import { getAllPosts } from "/lib/blogPosts";
-import images from "/components/Images.jsx"; 
+import images from "../components/imges";
 
 export default async function sitemap() {
   const baseUrl = "https://dillonwalsh.com";
 
-  const projects = await getAllProjects();
-  
-  const projectUrls = projects.map((project) => {
-    const projectImage = images.find((img) => img.id === project.id);
+  const projectUrls = images.map((project) => ({
+    url: `${baseUrl}/projects/${project.id}`,
+    lastModified: new Date(), 
+    changeFrequency: "monthly",
+    priority: 0.8,
+    images: [
+      {
+        loc: `${baseUrl}${project.src}`, 
+        title: project.title,
+      },
+    ],
+  }));
 
-    return {
-      url: `${baseUrl}/projects/${project.id}`,
-      lastModified: new Date(project.updatedAt),
-      changeFrequency: "monthly",
-      priority: 0.8,
-      images: projectImage ? [{
-        loc: `${baseUrl}${projectImage.src}`,
-        title: projectImage.title,
-      }] : [],
-    };
-  });
-
-  // Fetch all blog posts
   const posts = await getAllPosts();
   const postUrls = posts.map((post) => ({
     url: `${baseUrl}/blog/${post.slug}`,
@@ -31,7 +25,6 @@ export default async function sitemap() {
     priority: 0.7,
   }));
 
-  // Define static routes
   const routes = [
     "",
     "projects",
