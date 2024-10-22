da7c892f0ea63754f25a3eaf6d264145e74c2d48
dillonpw <dillonpwalsh10@gmail.com>
2024-10-22 14:37:24 -0400

    updated blog post

--- components/BlogPost.jsx
+++ components/BlogPost.jsx
@@ -4,8 +4,8 @@ const BlogPost = ({ title, date, excerpt, coverImage, children }) => {
   return (
     <article className="prose dark:prose-dark mx-auto mt-10">
       <h1 className="text-center text-4xl font-bold">{title}</h1>
-      <p className="text-center text-sm text-gray-500">{date}</p>
-      <p className="mt-2 text-center text-lg text-gray-700">{excerpt}</p>
+      <p className="text-center text-sm ">{date}</p>
+      <p className="mt-2 text-center text-lg dark:text-gray-100">{excerpt}</p>
       <div className="mt-8">{children}</div>
     </article>
   );
