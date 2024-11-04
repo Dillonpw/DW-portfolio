fbb93eff0dbb7ef4c45fe69bcb8edecaf1893366
dillonpw <dillonpwalsh10@gmail.com>
2024-11-04 10:20:58 -0500

    updated metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,11 +7,31 @@ import { ThemeProvider } from "../components/theme-provider";
 /** @type {import("next").Metadata} */
 export const metadata = {
   title: "Dillon Walsh - Web Developer",
-  description:
-    "Dillon Walsh- Professional Web Developer. Come see what I've been up to!",
+  description: "Dillon Walsh- Professional Web Developer. Come see what I've been up to!",
   openGraph: {
     title: "Dillon Walsh - Web Developer",
+    description: "Professional Web Developer showcasing projects and blog posts.",
+    url: "https://www.dillonwalsh.com",
+    siteName: "Dillon Walsh Portfolio",
+    images: [
+      {
+        url: "https://www.dillonwalsh.com/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Dillon Walsh - Web Developer",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
   },
+  twitter: {
+    card: "summary_large_image",
+    title: "Dillon Walsh - Web Developer",
+    description: "Professional Web Developer.",
+    creator: "@dillonpw",
+    images: "/twitter-img.png",
+  },
+
   keywords: [
     "Dillon",
     "Walsh",
